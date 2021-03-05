c23cb20ff73d19f52e5f3f38f94ec4daa4fd9074
Maciej <maciej@MacBook-Pro-Maciej.local>
2021-03-05 22:43:09 +0100

    heroku config

--- src/main/java/com/maciejscislowski/simpledatawarehouse/infrastructure/config/WebConfig.java
+++ src/main/java/com/maciejscislowski/simpledatawarehouse/infrastructure/config/WebConfig.java
@@ -1,21 +1,13 @@
 package com.maciejscislowski.simpledatawarehouse.infrastructure.config;
 
-import feign.auth.BasicAuthRequestInterceptor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 @Configuration
 class WebConfig implements WebMvcConfigurer {
-
-    @Value("${app.es.host}")
-    private String host;
 
     @Bean
     public MethodValidationPostProcessor methodValidationPostProcessor() {
@@ -25,19 +17,6 @@ class WebConfig implements WebMvcConfigurer {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        URI uri = null;
-        try {
-            uri = new URI(host);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e.getMessage());
-        }
-
-        final String[] userInfo = uri.getUserInfo().split(":");
-        return new BasicAuthRequestInterceptor(userInfo[0], userInfo[1]);
     }
 
 }
--- src/main/java/com/maciejscislowski/simpledatawarehouse/infrastructure/etl/ElasticsearchClient.java
+++ src/main/java/com/maciejscislowski/simpledatawarehouse/infrastructure/etl/ElasticsearchClient.java
@@ -1,43 +1,37 @@
 package com.maciejscislowski.simpledatawarehouse.infrastructure.etl;
 
 import com.maciejscislowski.simpledatawarehouse.application.etl.Querier;
-import lombok.RequiredArgsConstructor;
-import org.apache.http.client.utils.URIBuilder;
+import com.maciejscislowski.simpledatawarehouse.infrastructure.UrlCredentialsUtil;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import static java.lang.String.join;
 
-@RequiredArgsConstructor
 @Component
 class ElasticsearchClient implements Querier {
 
-    @Value("${app.es.host}")
-    private String host;
-
-    @Value("${app.es.data-index-name}")
-    private String dataIndexName;
-
     private final RestTemplate restTemplate;
+    private final String url;
+    private final byte[] auth;
+
+    public ElasticsearchClient(@Value("${app.es.host}") String host,
+                               @Value("${app.es.data-index-name}") String index,
+                               RestTemplate restTemplate) {
+        auth = Base64.encodeBase64(UrlCredentialsUtil.parseUrl(host).v1().getBytes());
+        url = join("/", UrlCredentialsUtil.parseUrl(host).v2(), index, "_search");
+        this.restTemplate = restTemplate;
+    }
 
     @Override
     public String query(String indexName, String query) {
-        URI uri = null;
-        try {
-            uri = new URIBuilder().setScheme(host.split(":")[0])
-                    .setHost(host.split("://")[1]).setPathSegments(dataIndexName, "_search").build();
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e.getMessage());
-        }
         HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        return restTemplate.getForObject(uri, String.class);
+        headers.setBasicAuth(new String(auth));
+        return restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class).getBody();
     }
 
 }
